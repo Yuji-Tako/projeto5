0f74ac6f0bc6f50a667819a1a61e10d0158204d9
Yuji_Tako <yuji.tcr@gmail.com>
2021-03-15 21:09:51 -0300

    Aula3

--- src/app/pages/talkwithus/talkwithus.module.ts
+++ src/app/pages/talkwithus/talkwithus.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { TalkwithusPageRoutingModule } from './talkwithus-routing.module';
-
 import { TalkwithusPage } from './talkwithus.page';
 
+// 1) Importa módulo Reactive Forms
+import {ReactiveFormsModule} from '@angular/forms';
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    TalkwithusPageRoutingModule
+    TalkwithusPageRoutingModule,
+    // 2) Importa ReactiveFormsModule
+    ReactiveFormsModule
   ],
   declarations: [TalkwithusPage]
 })
--- src/app/pages/talkwithus/talkwithus.page.ts
+++ src/app/pages/talkwithus/talkwithus.page.ts
@@ -1,5 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+// 1) Importa dependencia
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl
+} from'@angular/forms';
+import{AngularFirestore} from '@angular/fire/firestore';
+import{DatePipe} from '@angular/common';
+import { AlertController } from '@ionic/angular';
+
+
+// 6) Validação personalizda
+// Não permite campos somente com espaços
+export function removeSpaces(control:AbstractControl) {
+  if (control && control.value && !control.value.replace(/\s/g, '').length){
+
+  control.setValue('');
+  }
+  return null
+}
 @Component({
   selector: 'app-talkwithus',
   templateUrl: './talkwithus.page.html',
@@ -7,9 +28,96 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TalkwithusPage implements OnInit {
 
-  constructor() { }
+  // 3) Criar atributos
+  public contactForm: FormGroup; // Cria o formulário
+  pipe = new DatePipe('en_US'); // Formatador de datas
+
+  constructor(
+// 2) Injeta dependencias
+    public alert: AlertController,
+    public form: FormBuilder,
+    public firestore: AngularFirestore
+  ) { }
 
   ngOnInit() {
+
+// 4) Chama os campos do formulário
+    this.contactFormCreate();
+  }
+// 5) Cria os campos do formulário
+  contactFormCreate(){
+    this.contactForm = this.form.group({
+      date:[''],
+
+      name:[
+      '',Validators.compose([Validators.required,Validators.minLength(3),removeSpaces])
+          ],
+      
+      email:[
+      '',Validators.compose([Validators.required,Validators.email,removeSpaces])
+      ],
+      
+      subject:[
+        '',Validators.compose([Validators.required,Validators.minLength(5),removeSpaces])
+      ],
+
+      msg:[
+        '',Validators.compose([Validators.required,Validators.minLength(5),removeSpaces])
+      ],
+    });
+  }
+  // 7) Processa envio do formulário
+  contactSend() {
+
+    // Gera e formata a data de envio
+    this.contactForm.controls.date.setValue(
+      this.pipe.transform(Date.now(), 'yyyy-MM-dd HH:mm:ss')
+    );
+
+    // Teste
+    console.log(this.contactForm.value);
+
+    // Salva no Firestore
+    this.firestore.collection('contacts').add(this.contactForm.value)
+
+      // Se salvar
+      .then(
+        () => {
+
+          // Feedback
+          this.feedback();
+        }
+      )
+
+      // Se falhar
+      .catch(
+        (error) => {
+          alert(`Oooops! Algo deu errado! ${error}`);
+        }
+      );
   }
 
+  // 8) Popup de feedback
+  async feedback() {
+    const alert = await this.alert.create({
+      header: 'Dale!',
+      message: 'Seu contato foi enviado para Tales.',
+      buttons: [{
+        text: 'OK',
+        handler: () => {
+
+          // Reset do formulario, mantendo nome e e-mail já usados
+          this.contactForm.reset({
+            date: '',
+            name: this.contactForm.value.name.trim(),
+            email: this.contactForm.value.email.trim(),
+            subject: '',
+            message: ''
+          });
+        }
+      }]
+    });
+
+    await alert.present();
+  }
 }
